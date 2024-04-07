ec480e596f192de1a07bb0978db862536bb251c9
alex-pythonista <indrajit.alex@gmail.com>
2024-04-08 01:33:40 +0600

    cicd updated

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
 templates = Jinja2Templates(directory="templates")
 
 
